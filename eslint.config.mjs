3b9d125ebe3b7d8549ea9b46b244ed4f3f0f6278
Evan Purkhiser <evanpurkhiser@gmail.com>
2026-03-21 11:46:28 -0400

    Adopt shared Oxc formatter config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,12 @@
 import {common} from '@evanpurkhiser/eslint-config';
 
-export default [...common, {rules: {'prettier/prettier': 'off'}}];
+export default [
+  ...common,
+  {
+    rules: {
+      'prettier/prettier': 'off',
+      'simple-import-sort/imports': 'off',
+      'simple-import-sort/exports': 'off',
+    },
+  },
+];
